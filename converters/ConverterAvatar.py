283eef71644001ffdb33383e1b85b7f67dd33c4b
iperov <lepersorium@gmail.com>
2019-07-31 18:13:12 +0400

    1

--- converters/ConverterAvatar.py
+++ converters/ConverterAvatar.py
@@ -20,7 +20,9 @@ class ConverterAvatar(Converter):
         self.predictor_input_size = predictor_input_size
         
         #dummy predict and sleep, tensorflow caching kernels. If remove it, conversion speed will be x2 slower
-        predictor_func ( np.zeros ( (predictor_input_size,predictor_input_size,3), dtype=np.float32 ) )
+        predictor_func ( np.zeros ( (predictor_input_size,predictor_input_size,3), dtype=np.float32 ),
+                         np.zeros ( (predictor_input_size,predictor_input_size,3), dtype=np.float32 ),
+                         np.zeros ( (predictor_input_size,predictor_input_size,3), dtype=np.float32 ) )
         time.sleep(2)
 
         predictor_func_host, predictor_func = SubprocessFunctionCaller.make_pair(predictor_func)
@@ -32,20 +34,26 @@ class ConverterAvatar(Converter):
         self.predictor_func_host.obj.process_messages()
         
     #override
-    def cli_convert_face (self, img_bgr, img_face_landmarks, debug, **kwargs):
-        
-
-        img_size = img_bgr.shape[1], img_bgr.shape[0]
-
-        inp_size = self.predictor_input_size        
-        face_mat = LandmarksProcessor.get_transform_mat (img_face_landmarks, inp_size, face_type=FaceType.FULL_NO_ROTATION)
-
-        inp_face_bgr = cv2.warpAffine( img_bgr, face_mat, (inp_size, inp_size), flags=cv2.INTER_LANCZOS4 )
+    def cli_convert_face (self, f0, f0_lmrk, f1, f1_lmrk, f2, f2_lmrk, debug, **kwargs):
         if debug:
-            debugs = [inp_face_bgr.copy()]
-        prd_face_bgr = self.predictor_func ( inp_face_bgr )
+            debugs = []
+            
+        inp_size = self.predictor_input_size
+          
+        f0_mat = LandmarksProcessor.get_transform_mat (f0_lmrk, inp_size, face_type=FaceType.FULL_NO_ROTATION)
+        f1_mat = LandmarksProcessor.get_transform_mat (f1_lmrk, inp_size, face_type=FaceType.FULL_NO_ROTATION)
+        f2_mat = LandmarksProcessor.get_transform_mat (f2_lmrk, inp_size, face_type=FaceType.FULL_NO_ROTATION)
+        
+        inp_f0 = cv2.warpAffine( f0, f0_mat, (inp_size, inp_size), flags=cv2.INTER_CUBIC )
+        inp_f1 = cv2.warpAffine( f1, f1_mat, (inp_size, inp_size), flags=cv2.INTER_CUBIC )
+        inp_f2 = cv2.warpAffine( f2, f2_mat, (inp_size, inp_size), flags=cv2.INTER_CUBIC )
 
-        out_img = np.clip(prd_face_bgr, 0.0, 1.0)
+        prd_f = self.predictor_func ( inp_f0, inp_f1, inp_f2 )
+
+        out_img = np.clip(prd_f, 0.0, 1.0)
+
+        out_img = np.concatenate ( [cv2.resize ( f1, (prd_f.shape[1], prd_f.shape[0])  ), 
+                                out_img], axis=1 )
         
         if debug:
             debugs += [out_img.copy()]
