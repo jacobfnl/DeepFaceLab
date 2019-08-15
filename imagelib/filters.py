1ab86431369a915a3f196afe09309ee3ba2bd539
Jeremy Hummel <jhummel@onica.com>
2019-08-15 10:52:12 -0700

    Normalized filter

--- imagelib/filters.py
+++ imagelib/filters.py
@@ -1,13 +1,19 @@
 import cv2
 
 
-def high_pass_filter(img, ksize=3):
-    return cv2.Laplacian(img, cv2.CV_32F, ksize=ksize)
+def high_pass_filter(im, ksize=3):
+    im = cv2.Laplacian(im, cv2.CV_32F, ksize=ksize)
+    norm_image = cv2.normalize(im, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    return norm_image
 
 
-def sobel_x(img, ksize=3):
-    return cv2.Sobel(img, cv2.CV_32F, dx=1, dy=0, ksize=ksize)
+def sobel_x(im, ksize=3):
+    im = cv2.Sobel(im, cv2.CV_32F, dx=1, dy=0, ksize=ksize)
+    norm_image = cv2.normalize(im, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    return norm_image
 
 
-def sobel_y(img, ksize=3):
-    return cv2.Sobel(img, cv2.CV_32F, dx=0, dy=1, ksize=ksize)
+def sobel_y(im, ksize=3):
+    im = cv2.Sobel(im, cv2.CV_32F, dx=0, dy=1, ksize=ksize)
+    norm_image = cv2.normalize(im, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+    return norm_image
