6cb08640da73fff8a72586a828f585fee6d95899
shawn.oakley <shawnoakley@gmail.com>
2019-08-23 17:07:07 -0400

    Removes unnecessary comments

--- app/templates/static/js/currentArchitecture/currentArchitectureReducer.js
+++ app/templates/static/js/currentArchitecture/currentArchitectureReducer.js
@@ -1,6 +1,4 @@
 import { CurrentArchitectureActions } from '../constants/currentArchitectureConstants';
-// https://medium.com/@pie6k/better-way-to-create-type-safe-redux-actions-and-reducers-with-typescript-45386808c103
-
 
 export default function formStateReducer(state = null, action) {
 const {data} = action;
