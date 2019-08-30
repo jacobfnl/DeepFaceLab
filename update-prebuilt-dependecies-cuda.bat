551bd1da5ce12031bd551ac1b6b769892e10d80d
jh <jmhummel@gmail.com>
2019-08-30 05:18:19 +0000

    got the right wheel

--- update-prebuilt-dependecies-cuda.bat
+++ update-prebuilt-dependecies-cuda.bat
@@ -5,8 +5,6 @@ Set git=".\bin\git-cmd"
 
 python -c "from pip._internal import pep425tags; print('Supported:', pep425tags.get_supported(), '\n')"
 
-pip install --upgrade pip
-pip install .\bin\eos_py-1.1.2-cp36-cp36m-win32.whl
-pip install -r requirements-cuda.txt
+pip install .\bin\eos_py-1.1.2-cp36-cp36m-win_amd64.whl
 
 pause
