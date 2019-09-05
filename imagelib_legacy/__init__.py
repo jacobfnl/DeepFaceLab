73b0d5dd32cc8c1a9c5421d1f959b2375377213c
jh <jmhummel@gmail.com>
2019-09-05 16:17:38 -0700

    Removes more unneeded files for training

--- imagelib_legacy/__init__.py
+++
@@ -1,3 +0,0 @@
-from .color_transfer import color_hist_match
-from .color_transfer import reinhard_color_transfer
-from .color_transfer import linear_color_transfer
--- samplelib/__init__.py
+++ samplelib/__init__.py
@@ -5,5 +5,3 @@ from .SampleProcessor import SampleProcessor
 from .SampleGeneratorBase import SampleGeneratorBase
 from .SampleGeneratorFace import SampleGeneratorFace
 from .SampleGeneratorPingPong import SampleGeneratorPingPong
-from .SampleGeneratorFaceTemporal import SampleGeneratorFaceTemporal
-from .SampleGeneratorImageTemporal import SampleGeneratorImageTemporal
