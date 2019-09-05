11b76e87d6052f9fd61f555aecaefbb5cff4127e
jh <jmhummel@gmail.com>
2019-09-05 16:26:02 -0700

    Removes more files

--- facelib/__init__.py
+++ facelib/__init__.py
@@ -1,7 +1,1 @@
 from .FaceType import FaceType
-from .DLIBExtractor import DLIBExtractor
-from .MTCExtractor import MTCExtractor
-from .S3FDExtractor import S3FDExtractor
-from .LandmarksExtractor import LandmarksExtractor
-from .FANSegmentator import FANSegmentator
-from .PoseEstimator import PoseEstimator
