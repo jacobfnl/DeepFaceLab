6de4d24f538afe294e53c548cbeb0eea95eb77e0
Jacob Fennell <jacobfennell@jacobfennell.com>
2020-01-23 12:44:54 -0500

    upload the training to Talon

---
+++ train.py
@@ -0,0 +1,89 @@
+import os
+import shutil
+import multiprocessing
+
+from mainscripts import Trainer
+from mainscripts.Util import recover_original_aligned_filename
+from utils import Path_utils, os_utils, db_connection
+from interact import interact as io
+from pathlib import Path
+from utils.init_workspace import init_workspace
+import argparse
+
+workspace, src_aligned, dst_aligned, workspace_model = init_workspace()
+data_src = os.path.abspath(os.path.join(src_aligned, os.pardir))
+data_dst = os.path.abspath(os.path.join(dst_aligned, os.pardir))
+
+
+character_bucket = '/media/warriordata/character_bucket'
+live_faces = '/media/warriordata/live_faces'
+
+
+class fixPathAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, os.path.abspath(os.path.expanduser(values)))
+
+
+def process_train(training_data_src_dir,training_data_dst_dir, model_dir, debug_dir, force_gpu_idx):
+    os_utils.set_process_lowest_prio()
+    args = {'training_data_src_dir': training_data_src_dir,
+            'training_data_dst_dir': training_data_dst_dir,
+            'pretraining_data_dir': None,
+            'model_path': model_dir,
+            'model_name': 'SAEHD',
+            'no_preview': False,
+            'debug': debug_dir,
+            'flask_preview': True,
+            'execute_programs': []
+            }
+    device_args = {'cpu_only': False,
+                   'force_gpu_idx': force_gpu_idx,
+                   'use_fp16': False
+                   }
+    from mainscripts import Trainer
+    Trainer.main(args, device_args)
+
+
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method("spawn")
+    parser = argparse.ArgumentParser()
+
+    def process_args(args):
+        print(process_args.__name__)
+        os.environ.setdefault('DB_DB', 'WR')
+        os.environ.setdefault('DB_USER', '')
+        os.environ.setdefault('DB_PASS', '')
+        os.environ.setdefault('DB_ADDRESS', '')
+        live_person_uuid = args.live_person
+        link_id = args.link_id
+        gpu_idx = args.gpu_idx
+        training_data_dst_dir = os.path.join(character_bucket, link_id, 'chips')
+        model_dir = 'workspace/model_' + live_person_uuid
+
+        db = db_connection.open_db_connection()
+        query = f"SELECT COPYRIGHT FROM inconode where CONTENT_TYPE='extracted' AND DISTRIBUTION='{live_person_uuid}'"
+        cursor = db.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        yymmdd = ''
+        if len(result):
+            yymmdd = result[0][0]
+        if yymmdd == '':
+            print(f"\nError, could not find person: {live_person_uuid} \nPerhaps they've not been extracted, "
+                  f"or have a different CONTENT_TYPE code in the database.")
+            exit(-1)
+        training_data_src_dir = os.path.join(live_faces, yymmdd, live_person_uuid, 'chips_224')
+
+        process_train(training_data_src_dir=training_data_src_dir, training_data_dst_dir=training_data_dst_dir,
+                      model_dir=model_dir, force_gpu_idx=gpu_idx)
+
+    parser.add_argument('-person', required=True, dest="live_person",
+                        help="live_person uuid")
+    parser.add_argument('-link-id', required=True, dest="link_id",
+                        help="film character link_id")
+    parser.add_argument('-gpu', type=int, dest="gpu_idx", default=-1, help="Force to choose this GPU idx.")
+
+    parser.set_defaults(func=process_args)
+    arguments = parser.parse_args()
+    arguments.func(arguments)
